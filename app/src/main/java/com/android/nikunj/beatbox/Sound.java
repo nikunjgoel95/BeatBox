30ead3fe9d1ad2d6d11dd55476997b6f4bb8a382
nikunjgoel95 <nikunjgoel95@gmail.com>
2017-07-24 00:08:50 +0530

    Initial commit

---
+++ app/src/main/java/com/android/nikunj/beatbox/Sound.java
@@ -0,0 +1,35 @@
+package com.android.nikunj.beatbox;
+
+/**
+ * Created by nikunjgoel on 21-07-2017.
+ */
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+
+    public Sound(String assetPath)
+    {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav","");
+    }
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
